acbd1f0fc89c74f3c3d38d185ae4176309c0d84f
Jana Saa <jana.abusaa@gmail.com>
2024-12-15 10:51:39 +0200

    Add DbContext

---
+++ HotelBookingPlatform.Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using HotelBookingPlatform.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingPlatform.Infrastructure
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<FeaturedDeal> FeaturedDeals { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelImage> HotelImages { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomImage> RoomImages { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- HotelBookingPlatform.Infrastructure/Entities/Review.cs
+++ HotelBookingPlatform.Infrastructure/Entities/Review.cs
@@ -1,9 +1,4 @@
 ﻿using HotelBookingPlatform.Infrastructure.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelBookingPlatform.Infrastructure.Entities
 {
