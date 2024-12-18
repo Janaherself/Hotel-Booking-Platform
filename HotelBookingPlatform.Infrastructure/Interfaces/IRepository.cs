260c470f58b857c599c4b1f99e40165e6fc720e8
Jana Saa <jana.abusaa@gmail.com>
2024-12-19 01:12:16 +0200

    Add generic repository

---
+++ HotelBookingPlatform.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingPlatform.Infrastructure.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(int id);
+        Task<List<TEntity>> GetAllAsync(int pageSize = 6, int pageNumer = 1);
+        void Delete(TEntity item);
+        void Update(TEntity item);
+        Task AddAsync(TEntity item);
+        Task SaveAsync();
+    }
+}
---
+++ HotelBookingPlatform.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using HotelBookingPlatform.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingPlatform.Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(int pageSize = 6, int pageNumber = 1)
+        {
+            return await _dbSet
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public void Delete(TEntity item)
+        {
+            _dbSet.Remove(item);
+        }
+
+        public void Update(TEntity item)
+        {
+            _dbSet.Update(item);
+        }
+
+        public async Task AddAsync(TEntity item)
+        {
+            await _dbSet.AddAsync(item);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
