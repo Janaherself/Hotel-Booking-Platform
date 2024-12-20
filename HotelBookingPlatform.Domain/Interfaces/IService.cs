da6320e59352e81725c70c887c45ba894cbf697a
Jana Saa <jana.abusaa@gmail.com>
2024-12-20 13:24:40 +0200

    Add generic service

---
+++ HotelBookingPlatform.Domain/Interfaces/IService.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingPlatform.Domain.Interfaces
+{
+    public interface IService<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync(int pageSize = 6, int pageNumer = 1);
+        void Delete(T item);
+        void Update(T item);
+        Task AddAsync(T item);
+        Task SaveAsync();
+    }
+}
---
+++ HotelBookingPlatform.Domain/Services/Service.cs
@@ -0,0 +1,37 @@
+﻿using HotelBookingPlatform.Domain.Interfaces;
+
+namespace HotelBookingPlatform.Domain.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        public Task AddAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync(int pageSize = 6, int pageNumer = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
