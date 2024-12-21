7f5b036c61e65568092f08162f6b0246e4bc2119
Jana Saa <jana.abusaa@gmail.com>
2024-12-21 21:08:20 +0200

    Add generic service

--- HotelBookingPlatform.Domain/Services/Service.cs
+++ HotelBookingPlatform.Domain/Services/Service.cs
@@ -4,52 +4,43 @@ using HotelBookingPlatform.Infrastructure.Interfaces;
 
 namespace HotelBookingPlatform.Domain.Services
 {
-    public class Service<T, TEntity> : IService<T, TEntity> 
+    public class Service<T, TEntity>(IRepository<TEntity> repository, IMapper mapper) : IService<T, TEntity> 
         where T : class 
         where TEntity : class
     {
-        private readonly IRepository<TEntity> _repository;
-        private readonly IMapper _mapper;
-
-        public Service(IRepository<TEntity> repository, IMapper mapper)
-        {
-            _repository = repository;
-            _mapper = mapper;
-        }
-
         public async Task AddAsync(T item)
         {
-            var entity = _mapper.Map<TEntity>(item);
-            await _repository.AddAsync(entity);
+            var entity = mapper.Map<TEntity>(item);
+            await repository.AddAsync(entity);
         }
 
         public void Delete(T item)
         {
-            var entity = _mapper.Map<TEntity>(item);
-            _repository.Delete(entity);
+            var entity = mapper.Map<TEntity>(item);
+            repository.Delete(entity);
         }
 
         public async Task<IEnumerable<T>> GetAllAsync(int pageSize = 6, int pageNumer = 1)
         {
-            var entities = await _repository.GetAllAsync(pageSize, pageNumer);
-            return _mapper.Map<IEnumerable<T>>(entities);
+            var entities = await repository.GetAllAsync(pageSize, pageNumer);
+            return mapper.Map<IEnumerable<T>>(entities);
         }
 
         public async Task<T?> GetByIdAsync(int id)
         {
-            var entity = await _repository.GetByIdAsync(id);
-            return _mapper.Map<T>(entity);
+            var entity = await repository.GetByIdAsync(id);
+            return mapper.Map<T>(entity);
         }
 
         public async Task SaveAsync()
         {
-            await _repository.SaveAsync();
+            await repository.SaveAsync();
         }
 
         public void Update(T item)
         {
-            var entity = _mapper.Map<TEntity>(item);
-            _repository.Update(entity);
+            var entity = mapper.Map<TEntity>(item);
+            repository.Update(entity);
         }
     }
 }
