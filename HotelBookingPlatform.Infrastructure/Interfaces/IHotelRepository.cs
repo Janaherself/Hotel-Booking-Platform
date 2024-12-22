3819df99689a83683bc0539529550cd51b75e13f
Jana Saa <jana.abusaa@gmail.com>
2024-12-22 10:51:04 +0200

    Add special repositories interfaces

---
+++ HotelBookingPlatform.Infrastructure/Interfaces/IHotelRepository.cs
@@ -0,0 +1,11 @@
+﻿using HotelBookingPlatform.Infrastructure.Entities;
+
+namespace HotelBookingPlatform.Infrastructure.Interfaces
+{
+    public interface IHotelRepository : IRepository<Hotel>
+    {
+        Task<IEnumerable<Hotel>> SearchByRatingAsync(float rating);
+        Task<IEnumerable<Hotel>> SearchByAmenitiesAsync(string amenities);
+        Task<IEnumerable<Hotel>> SearchByCityAsync(string city);
+    }
+}
---
+++ HotelBookingPlatform.Infrastructure/Interfaces/IRoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using HotelBookingPlatform.Infrastructure.Entities;
+
+namespace HotelBookingPlatform.Infrastructure.Interfaces
+{
+    public interface IRoomRepository : IRepository<Room>
+    {
+        Task<IEnumerable<Room>> SearchByPriceAsync(decimal price);
+        Task<IEnumerable<Room>> SearchByAdultCapacityAsync(int adults);
+        Task<IEnumerable<Room>> SearchByChildrenCapacityAsync(int children);
+        Task<IEnumerable<Room>> SearchByTypeAsync(string type);
+    }
+}
