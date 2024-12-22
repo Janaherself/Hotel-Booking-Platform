37c2d6c417278e2d50868380cb8de90fa26d26a1
Jana Saa <jana.abusaa@gmail.com>
2024-12-22 10:58:07 +0200

    Register current services in DI container

--- HotelBookingPlatform.App/Program.cs
+++ HotelBookingPlatform.App/Program.cs
@@ -1,15 +1,39 @@
+using HotelBookingPlatform.Domain.Interfaces;
+using HotelBookingPlatform.Domain.Services;
+using HotelBookingPlatform.Infrastructure;
+using HotelBookingPlatform.Infrastructure.Interfaces;
+using HotelBookingPlatform.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultDB"))
+    .EnableSensitiveDataLogging();
+});
+
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IBookinglRepository, BookingRepository>();
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+
+builder.Services.AddScoped(typeof(IService<,>), typeof(Service<,>));
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddScoped<IHotelService, HotelService>();
+builder.Services.AddScoped<IRoomService, RoomService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
