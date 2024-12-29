00df2c2946e04610548e85c8fe31b0efe90ba1d1
Jana Saa <jana.abusaa@gmail.com>
2024-12-29 21:37:53 +0200

    Create custom InvalidRoomIdException

---
+++ HotelBookingPlatform.Domain/Exceptions/InvalidRoomIdException.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelBookingPlatform.Domain.Exceptions
+{
+    class InvalidRoomIdException : Exception
+    {
+        public InvalidRoomIdException() : base("One or more RoomIds are invalid.")
+        {   
+        }
+
+        public InvalidRoomIdException(string message) : base(message)
+        {   
+        }
+
+        public InvalidRoomIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
