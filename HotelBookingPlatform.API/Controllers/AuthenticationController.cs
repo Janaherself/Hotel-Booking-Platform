b51442db5324f4aee9c172f771e569e21d0c5728
Jana Saa <jana.abusaa@gmail.com>
2024-12-29 18:10:44 +0200

    Create authentication controller

---
+++ HotelBookingPlatform.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using HotelBookingPlatform.API.DTOs;
+using HotelBookingPlatform.Domain.DomainEntities;
+using HotelBookingPlatform.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingPlatform.API.Controllers
+{
+    /// <summary>
+    /// Handles the logic of authenticating users (login/register)
+    /// </summary>
+    /// <param name="userService"></param>
+    /// <param name="mapper"></param>
+    /// <param name="logger"></param>
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController(IUserService userService, IMapper mapper, ILogger logger) : ControllerBase
+    {
+        /// <summary>
+        /// Signs in an existing user to the API
+        /// </summary>
+        /// <param name="loginDto"></param>
+        /// <returns></returns>
+        [Route("login")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            var login = mapper.Map<LoginEntity>(loginDto);
+
+            logger.LogInformation("Calling the login method in the user service..");
+            var userToken = userService.LoginAsync(login).ToString();
+            if (userToken == null)
+            {
+                logger.LogInformation("User with email {Email} couldn't be authenticated.", login.Email);
+                return Unauthorized();
+            }
+
+            logger.LogInformation("Returning the token back to user..");
+            return Ok(userToken);
+        }
+
+        /// <summary>
+        /// Signs up a new user in the API
+        /// </summary>
+        /// <param name="loginDto"></param>
+        /// <returns></returns>
+        [Route("register")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult Register([FromBody] LoginDto loginDto)
+        {
+            var login = mapper.Map<LoginEntity>(loginDto);
+
+            logger.LogInformation("Calling the register method in the user service..");
+            var userToken = userService.RegisterAsync(login).ToString();
+            if (userToken == null)
+            {
+                logger.LogInformation("User with email {Email} couldn't be registered.", login.Email);
+                return Unauthorized();
+            }
+
+            logger.LogInformation("Returning the token back to user..");
+            return Ok(userToken);
+        }
+    }
+}
