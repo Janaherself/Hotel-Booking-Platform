023f9a942eb2f3b01f171f983d5d01d19d72a38a
Jana Saa <jana.abusaa@gmail.com>
2025-01-09 12:48:51 +0200

    Create integration tests for AuthenticationController

---
+++ HotelBookingPlatform.API.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using HotelBookingPlatform.Domain.Interfaces;
+using HotelBookingPlatform.Domain.Services;
+using HotelBookingPlatform.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBookingPlatform.API.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+
+                services.AddSingleton<ITokenService, TokenService>();
+
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+                using var appContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                appContext.Database.EnsureCreated();
+            });
+        }
+    }
+
+}
